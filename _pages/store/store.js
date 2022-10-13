9433677c624c4846d08466ced3e081bc048546fb
georgeclassgit <104859831+georgeclassgit@users.noreply.github.com>
2022-10-13 23:55:26 +0500

    Add files via upload

---
+++ _pages/store/store.js
@@ -0,0 +1,17 @@
+.button {
+    font-family: Inter;
+    font-size: 14px;
+    font-weight: 500;
+    border: none;
+    border-radius: 4px;
+    background-color: inherit;
+    color: #fff;
+}
+
+.button--purple {
+    background-color: #8312F5;
+}
+
+.button--stroke {
+   outline: 1px solid #fff;
+}
